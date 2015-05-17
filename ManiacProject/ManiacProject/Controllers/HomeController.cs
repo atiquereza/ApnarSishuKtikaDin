3b9bf793f108235a00530c053eb86f58baf5d355
Jubayer <aljubayer@gmail.com>
2015-05-17 20:21:56 +0600

    Web Application

---
+++ ManiacProject/ManiacProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ManiacProject.Libs;
+
+namespace ManiacProject.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            if(Session.Count == 0)
+            {
+                return RedirectToAction("Index", "Account");
+  
+            }
+
+            Dictionary<string, string> sessionData = SessionHandler.GetSessionData(Session);
+            if (Authentication.Authenticate(sessionData, Session))
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Account");
+            
+        }
+
+    }
+}
---
+++ ManiacProject/ManiacProject/Views/Classes/TemplateItems.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ManiacProject.Libs;
+
+namespace ManiacProject.Views.Classes
+{
+    public static class TemplateItems
+    {
+        //public static string WelcomeMessage = "Welcome to O&M Supervision";
+
+
+        public static string GetWelcomeStringView()
+        {
+            string WelcomeMessage = "";//;"Welcome to OMCB";
+            string htmlData = string.Empty;
+
+            htmlData += "<div class=\"center\"><a><h>" + WelcomeMessage;
+            htmlData += "</h></a></div>";
+
+            return htmlData;
+        }
+        public static string GetHeaderLogInView(HttpSessionStateBase Session)
+        {
+            string htmlData = string.Empty;
+            if(Session.Count > 0)
+            {
+                Dictionary<string, string> sessionData = SessionHandler.GetSessionData(Session);
+                htmlData += "<div style=\"text-align:right;\">";
+                htmlData += "<a>Logged In as: " + sessionData["LoggedIn"] + "</a><br>";
+                htmlData += "</div>";
+            }
+
+            return htmlData;
+        }
+    }
+}
